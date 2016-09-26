822e486409a357c6c1d0c7e720f72b48d1ea2c2b
Your Name <you@example.com>
2016-09-26 16:46:50 +0300

    Внесение исправлений по замечаниям

--- bars.py
+++ bars.py
@@ -1,71 +1,43 @@
 import json
-import math
+import os
+import sys
 
 
 def load_data(filepath):
-    return json.load(open(filepath, 'r'))
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
 
 
-def get_biggest_bar(data):
-    i = 0
-    max_seats = 0
-    sp_max = []
-
-    while i < len(data):
-        if data[i]["Cells"]["SeatsCount"] == data[
-                max_seats]["Cells"]["SeatsCount"]:
-            sp_max.append(data[i]["Cells"]["Name"])
-        elif data[i]["Cells"]["SeatsCount"] > data[max_seats][
-                "Cells"]["SeatsCount"]:
-            max_seats = i
-            sp_max = []
-            # нашли больший бар, очищаем список больших баров
-            sp_max.append(data[i]["Cells"]["Name"])
-        i = i + 1
-    return sp_max
+def get_distance(my_longitude, my_latitude, bar_longitude, bar_latitude):
+    return ((my_longitude - bar_longitude)**2 + (my_latitude - bar_latitude)**2)**0.5
 
 
-def get_smallest_bar(data):
-    i = 0
-    min_seats = 0
-    sp_min = []
+def get_bar(data, bar_type):
+    if bar_type == 'big':
+        sorted_bars = sorted(data, key=lambda bar: bar['Cells']['SeatsCount'], reverse=True)
+        mark_of_bar = sorted_bars[0]['Cells']['SeatsCount']
+    elif bar_type == 'small':
+        sorted_bars = sorted(data, key=lambda bar: bar['Cells']['SeatsCount'])
+        mark_of_bar = sorted_bars[0]['Cells']['SeatsCount']
+    elif bar_type == 'near':
+        sorted_bars = sorted(data, key=lambda bar: get_distance(my_longitude, my_latitude, bar["Cells"]["geoData"]["coordinates"][0], bar["Cells"]["geoData"]["coordinates"][1]))
+        mark_of_bar = get_distance(my_longitude, my_latitude, sorted_bars[0]["Cells"]["geoData"]["coordinates"][0], sorted_bars[0]["Cells"]["geoData"]["coordinates"][1])
+    else:
+        return None
 
-    while i < len(data):
-        if data[i]["Cells"]["SeatsCount"] == data[
-                min_seats]["Cells"]["SeatsCount"]:
-            sp_min.append(data[i]["Cells"]["Name"])
-        elif data[i]["Cells"]["SeatsCount"] < data[min_seats][
-                "Cells"]["SeatsCount"]:
-            min_seats = i
-            sp_min = []
-            # нашли меньший бар, очищаем список малых баров
-            sp_min.append(data[i]["Cells"]["Name"])
-        i = i + 1
-    return sp_min
-
-
-def get_closest_bar(data, longitude, latitude):
-    i = 0
-    min_dlina = math.sqrt((float(data[0]["Cells"]["geoData"]["coordinates"][
-                          0]) - longitude)**2 + (float(data[0]["Cells"][
-                              "geoData"]["coordinates"][1]) - latitude)**2)
-    sp_dl = []
-
-    while i < len(data):
-        dlina = math.sqrt((float(data[i]["Cells"]["geoData"]["coordinates"][
-                          0]) - longitude)**2 + (float(data[i]["Cells"][
-                              "geoData"]["coordinates"][1]) - latitude)**2)
-        if min_dlina == dlina:
-            sp_dl.append(data[i]["Cells"]["Name"])
-        elif dlina < min_dlina:
-            min_dlina = dlina
-            sp_dl = []
-            # нашли ближайший бар, очищаем список близких баров
-            sp_dl.append(data[i]["Cells"]["Name"])
-        i = i + 1
-    return sp_dl
-
-# проверка введенного числа
+    number_of_seats = sorted_bars[0]['Cells']['SeatsCount']
+    name_of_bar = []
+    for bar in sorted_bars:
+        if bar_type == 'near':
+            if mark_of_bar != get_distance(my_longitude, my_latitude, bar["Cells"]["geoData"]["coordinates"][0], bar["Cells"]["geoData"]["coordinates"][1]):
+                break
+        else:
+            if mark_of_bar != bar['Cells']['SeatsCount']:
+                break
+        name_of_bar.append(bar["Cells"]["Name"])
+    return name_of_bar, mark_of_bar
 
 
 def is_digit(string):
@@ -80,36 +52,28 @@ def is_digit(string):
 
 
 if __name__ == '__main__':
-    # ввод GPS-координат
-    print('Здравствуйте!')
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--help':
+            print("Скрипт находит самый большой, маленький и ближайший бары из JSON-файла.")
+            print("Введите в терминале: python3.5 bars.py file.json")
+        else:
+            bars_json = load_data(sys.argv[1])
+            if bars_json is None:
+                print("JSON-файл не обнаружен!")
+                sys.exit
+            else:
+                my_latitude = input("Пожалуйста, введите координаты широты вашего местоположения: ")
+                while not is_digit(my_latitude):
+                    my_latitude = input("Широта введена неверно, повторите ввод: ")
+                my_latitude = float(my_latitude.replace(',', '.'))
 
-    x_gps = input(
-        "Пожалуйста, введите координаты широты вашего местоположения: ")
-    while not is_digit(x_gps):
-        x_gps = input("Широта введена неверно, повторите ввод: ")
-    x_gps = float(x_gps.replace(',', '.'))
+                my_longitude = input("Пожалуйста, введите координаты долготы вашего местоположения: ")
+                while not is_digit(my_longitude):
+                    my_longitude = input("Долгота введена неверно, повторите ввод: ")
+                my_longitude = float(my_longitude.replace(',', '.'))
 
-    y_gps = input(
-        "Пожалуйста, введите координаты долготы вашего местоположения: ")
-    while not is_digit(y_gps):
-        y_gps = input("Долгота введена неверно, повторите ввод: ")
-    y_gps = float(y_gps.replace(',', '.'))
-
-# чтение json-файла
-    data = load_data('Бары.json')
-
-# вывод результатов
-    if len(get_biggest_bar(data)) < 2:
-        print('Самый большой бар - ', get_biggest_bar(data)[0])
+                print('Самый(ые) большой(ые) бар(ы):', get_bar(bars_json, 'big')[0], ', число посадочных мест:', get_bar(bars_json, 'big')[1])
+                print('Самый(ые) маленький(ие) бар(ы):', get_bar(bars_json, 'small')[0], ', число посадочных мест:', get_bar(bars_json, 'small')[1])
+                print('Самый(ые) ближайший(ие) бар(ы):', get_bar(bars_json, 'near')[0], ', расстояние до бара(ов):', get_bar(bars_json, 'near')[1])
     else:
-        print('Самые большие бары - ', get_biggest_bar(data))
-
-    if len(get_smallest_bar(data)) < 2:
-        print('Самый маленький бар - ', get_smallest_bar(data)[0])
-    else:
-        print('Самые маленькие бары - ', get_smallest_bar(data))
-
-    if len(get_closest_bar(data, x_gps, y_gps)) < 2:
-        print('Самый ближайший бар - ', get_closest_bar(data, x_gps, y_gps)[0])
-    else:
-        print('Самые близкие бары - ', get_closest_bar(data, x_gps, y_gps))
+        print("Не задан JSON-файл для обработки!")
