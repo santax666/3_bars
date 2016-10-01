5189a31a988a1f919f3da69303695177090ae904
Your Name <you@example.com>
2016-10-01 20:57:11 +0300

    Оптимизация кода

--- bars.py
+++ bars.py
@@ -1,6 +1,7 @@
 import json
 import os
-import sys
+import argparse
+from math import hypot
 
 
 def load_data(filepath):
@@ -10,33 +11,40 @@ def load_data(filepath):
         return json.load(file_handler)
 
 
-def get_distance(my_longitude, my_latitude, bar_longitude, bar_latitude):
-    return ((my_longitude - bar_longitude)**2 + (my_latitude - bar_latitude)**2)**0.5
+def get_distance(longitude, latitude, bar_coordinates):
+    return hypot(
+        (longitude - bar_coordinates[0]), (latitude - bar_coordinates[1]))
 
 
-def get_bar(data, bar_type, my_longitude, my_latitude):
-    if bar_type == 'big':
-        sorted_bars = sorted(data, key=lambda bar: bar['Cells']['SeatsCount'], reverse=True)
-        mark_of_bar = sorted_bars[0]['Cells']['SeatsCount']
-    elif bar_type == 'small':
-        sorted_bars = sorted(data, key=lambda bar: bar['Cells']['SeatsCount'])
-        mark_of_bar = sorted_bars[0]['Cells']['SeatsCount']
-    elif bar_type == 'near':
-        sorted_bars = sorted(data, key=lambda bar: get_distance(my_longitude, my_latitude, bar["Cells"]["geoData"]["coordinates"][0], bar["Cells"]["geoData"]["coordinates"][1]))
-        mark_of_bar = get_distance(my_longitude, my_latitude, sorted_bars[0]["Cells"]["geoData"]["coordinates"][0], sorted_bars[0]["Cells"]["geoData"]["coordinates"][1])
-    else:
-        return None
+def get_parse_data(data, longitude, latitude):
+    parse_data = []
+    for bar in data:
+        name = bar["Cells"]["Name"]
+        seats = bar["Cells"]['SeatsCount']
+        distance = get_distance(longitude, latitude,
+                                bar["Cells"]["geoData"]["coordinates"])
+        bar_info = (name, seats, distance,)
+        parse_data.append(bar_info)
+    return parse_data
 
-    number_of_seats = sorted_bars[0]['Cells']['SeatsCount']
+
+def set_preset_options(bar_type):
+    preset_options = {
+        'big': (
+            True, 1), 'small': (
+            False, 1), 'near': (
+                False, 2)}
+    return preset_options[bar_type]
+
+
+def get_bar(data, options):
     name_of_bar = []
-    for bar in sorted_bars:
-        if bar_type == 'near':
-            if mark_of_bar != get_distance(my_longitude, my_latitude, bar["Cells"]["geoData"]["coordinates"][0], bar["Cells"]["geoData"]["coordinates"][1]):
-                break
-        else:
-            if mark_of_bar != bar['Cells']['SeatsCount']:
-                break
-        name_of_bar.append(bar["Cells"]["Name"])
+    data.sort(key=lambda bar: bar[options[1]], reverse=options[0])
+    for bar in data:
+        if bar[options[1]] != data[0][options[1]]:
+            break
+        name_of_bar.append(bar[0])
+    mark_of_bar = data[0][options[1]]
     return name_of_bar, mark_of_bar
 
 
@@ -51,29 +59,42 @@ def is_digit(string):
             return False
 
 
+def createParser():
+    parser = argparse.ArgumentParser(usage='%(prog)s [аргументы]',
+                                     description="Определение самого большого,"
+                                                 " маленького, близкого бара"
+                                                 " с помощью %(prog)s")
+    parser.add_argument("jsonfile", help="JSON-файл для обработки")
+    return parser
+
+
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if sys.argv[1] == '--help':
-            print("Скрипт находит самый большой, маленький и ближайший бары из JSON-файла.")
-            print("Введите в терминале: python3.5 bars.py file.json")
-        else:
-            bars_json = load_data(sys.argv[1])
-            if bars_json is None:
-                print("JSON-файл не обнаружен!")
-                sys.exit
-            else:
-                my_latitude = input("Пожалуйста, введите координаты широты вашего местоположения: ")
-                while not is_digit(my_latitude):
-                    my_latitude = input("Широта введена неверно, повторите ввод: ")
-                my_latitude = float(my_latitude.replace(',', '.'))
+    parser = createParser()
+    namespace = parser.parse_args()
+    json_file = namespace.jsonfile
 
-                my_longitude = input("Пожалуйста, введите координаты долготы вашего местоположения: ")
-                while not is_digit(my_longitude):
-                    my_longitude = input("Долгота введена неверно, повторите ввод: ")
-                my_longitude = float(my_longitude.replace(',', '.'))
-
-                print('Самый(ые) большой(ые) бар(ы):', get_bar(bars_json, 'big',my_longitude, my_latitude)[0], ', число посадочных мест:', get_bar(bars_json, 'big',my_longitude, my_latitude)[1])
-                print('Самый(ые) маленький(ие) бар(ы):', get_bar(bars_json, 'small',my_longitude, my_latitude)[0], ', число посадочных мест:', get_bar(bars_json, 'small',my_longitude, my_latitude)[1])
-                print('Самый(ые) ближайший(ие) бар(ы):', get_bar(bars_json, 'near',my_longitude, my_latitude)[0], ', расстояние до бара(ов):', get_bar(bars_json, 'near',my_longitude, my_latitude)[1])
+    jsonfile = load_data(json_file)
+    if jsonfile is None:
+        print("JSON-файл не обнаружен!")
+        sys.exit
     else:
-        print("Не задан JSON-файл для обработки!")
+        latitude = input("Пожалуйста, введите вашу широту: ")
+        while not is_digit(latitude):
+            latitude = input("Неверная широта, повторите ввод: ")
+        latitude = float(latitude.replace(',', '.'))
+
+        longitude = input("Пожалуйста, введите вашу долготу: ")
+        while not is_digit(longitude):
+            longitude = input("Неверная долгота, повторите ввод: ")
+        longitude = float(longitude.replace(',', '.'))
+
+        bars_data = get_parse_data(jsonfile, longitude, latitude)
+
+        data_print = [['большой(ые)', 'big', ', посадочных мест:'],
+                      ['маленький(ие)', 'small', ', посадочных мест:'],
+                      ['ближайший(ие)', 'near', ', расстояние до бара(ов):']]
+        for data in data_print:
+            options = set_preset_options(data[1])
+            print('Самый(ые)', data[0], 'бар(ы):',
+                  get_bar(bars_data, options)[0], data[2],
+                  get_bar(bars_data, options)[1])
